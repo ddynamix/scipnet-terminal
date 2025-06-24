d6f49d10739c6c292616cae471e4e8e4fa960a39
Tyler Steptoe <tytysteptoe@gmail.com>
2025-06-24 16:16:35 -0400

    fixed newline split

--- scp_formatter.py
+++ scp_formatter.py
@@ -4,22 +4,32 @@ import re
 term = Terminal()
 
 def clean_brackets(text: str):
-    # First handle triple brackets - convert to color format
+    # Handle triple brackets for SCP links - convert to color format
     text = re.sub(r'\[\[\[(.*?)\]\]\]', r'{term.firebrick}\1{term.normal}', text)
 
-    # Then remove regular double brackets and their content
+    # remove regular double brackets and their content
     text = re.sub(r'\[\[(.*?)\]\]', '', text)
+
+    # remove multiline double bracketed text
+    if text[0:2] == "[[" or text[-2:] == "]]":
+        text = ""
 
     return text
 
 
 def formatted_print(metadata, scp: str):
-    lines = scp.split("<\p>")
+    lines = scp.split("\n")
+    lines = [x for x in lines if x.strip()]
+
     for line in lines:
+        # End of article
+        if "[[footnoteblock]]" in line:
+            break
+
         # Remove all meta-elements of the page
         line = clean_brackets(line)
 
         # Bold any double-astrixed text
         line = re.sub(r'\*\*(.*?)\*\*', r'{term.black_on_firebrick}\1{term.normal}', line)
 
-        print(line.format(term=term))
+        print(line.format(term=term) + "\n")

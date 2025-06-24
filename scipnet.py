c8aeb3cbe7f35a40053f4d4160cb1a0c3b7639c4
Tyler Steptoe <tytysteptoe@gmail.com>
2025-06-24 18:47:02 -0400

    added splash screen and improved formatting

--- scipnet.py
+++ scipnet.py
@@ -7,14 +7,17 @@ import scp_formatter
 base_url = "https://raw.githubusercontent.com/scp-data/scp-api/main/docs/data/scp/"
 term = Terminal()
 
-def successful_response(scpnum, response):
+def successful_response(scpnum, response):    
     print(term.yellow("Successfully retrieved SCP data"))
     response_json = response.json()
-    metadata_json = requests.get(base_url + "items/index.json").json()
     scp = response_json.get(f"SCP-{scpnum}").get("raw_source")
 
-    scp_formatter.formatted_print(metadata_json, scp)
+    metadata_json = requests.get(base_url + "items/index.json").json()
+    metadata = metadata_json.get(f"SCP-{scpnum}")
 
+    print(term.home + term.clear)
+    scp_formatter.formatted_print(metadata, scp)
+    
 
 def normalize_scp_id(user_input):
     match = re.match(r'(?i)(?:scp[\s\-]*)?(\d+)', user_input.strip())
@@ -25,52 +28,76 @@ def normalize_scp_id(user_input):
         return None
 
 
-print(term.home + term.clear)
-print(term.green("Welcome Researcher!"))
-command = input("What scp would you like to access? ")
+def main():
+    """
+    Program entry point
+    """
 
-if re.match(r'(?i)(?:scp[\s\-]*)?(\d+)', command.strip()):
-    scpnum = normalize_scp_id(command)
-    scpnum_1 = scpnum[0]
-    scpnum_2 = scpnum[1]
+    print(term.home + term.clear)
 
-    match scpnum_1:
-        case _ if scpnum_1 == "0" or len(scpnum) < 4:
-            if scpnum == "001":
-                print(term.red("Accessing SCP-001"))
-            else:
-                print(term.green("Accessing series 1"))
-                response = requests.get(base_url + "items/content_series-1.json")
+    # Print scp logo
+    f = open("scp_logo_small.txt", "r")
+    logo = f.read()
+    logo_lines = logo.split("\n")
+    for line in logo_lines:
+        line = line.strip()
+        print(term.center(line))
+    f.close()
+
+    # Print welcome text
+    f = open("welcome_text.txt", "r")
+    logo = f.read()
+    logo_lines = logo.split("\n")
+    for line in logo_lines:
+        print(term.center(term.firebrick_reverse(line), fillchar=" "))
+    f.close()
+
+    print(term.green("> Welcome Researcher!"))
+    command = input(f"> {term.blink}What scp would you like to access? {term.normal}")
+
+    if re.match(r'(?i)(?:scp[\s\-]*)?(\d+)', command.strip()):
+        scpnum = normalize_scp_id(command)
+        scpnum_1 = scpnum[0]
+        scpnum_2 = scpnum[1]
+
+        match scpnum_1:
+            case _ if scpnum_1 == "0" or len(scpnum) < 4:
+                if scpnum == "001":
+                    print(term.red("Accessing SCP-001"))
+                else:
+                    print(term.green("Accessing series 1"))
+                    response = requests.get(base_url + "items/content_series-1.json")
+                    if response.status_code == 200:
+                        successful_response(scpnum, response)
+                    else:
+                        print(term.red(f"Failed to fetch SCP"))
+
+            case "1" | "2" | "3" | "4" | "5":
+                series_num = str(int(scpnum_1) + 1)
+                print(term.green("Accessing series " + series_num))
+                response = requests.get(base_url + f"items/content_series-{series_num}.json")
                 if response.status_code == 200:
                     successful_response(scpnum, response)
                 else:
                     print(term.red(f"Failed to fetch SCP"))
 
-        case "1" | "2" | "3" | "4" | "5":
-            series_num = str(int(scpnum_1) + 1)
-            print(term.green("Accessing series " + series_num))
-            response = requests.get(base_url + f"items/content_series-{series_num}.json")
-            if response.status_code == 200:
-                successful_response(scpnum, response)
-            else:
-                print(term.red(f"Failed to fetch SCP"))
+            case "6" | "7" | "8" | "9":
+                series_num = str(int(scpnum_1) + 1)
+                print(term.green("Accessing series " + series_num))
 
-        case "6" | "7" | "8" | "9":
-            series_num = str(int(scpnum_1) + 1)
-            print(term.green("Accessing series " + series_num))
+                half_series_num = "0"
+                if int(scpnum_2) > 5:
+                    half_series_num = "5"
 
-            half_series_num = "0"
-            if int(scpnum_2) > 5:
-                half_series_num = "5"
+                response = requests.get(base_url + f"items/content_series-{series_num}.{half_series_num}.json")
+                if response.status_code == 200:
+                    successful_response(scpnum, response)
+                else:
+                    print(term.red(f"Failed to fetch SCP"))
 
-            response = requests.get(base_url + f"items/content_series-{series_num}.{half_series_num}.json")
-            if response.status_code == 200:
-                successful_response(scpnum, response)
-            else:
-                print(term.red(f"Failed to fetch SCP"))
-
-else:
-    print(term.red("Invalid scp id"))
+    else:
+        print(term.red("Invalid scp id"))
 
 
-
+if __name__ == "__main__":
+    main()
--- scp_formatter.py
+++ scp_formatter.py
@@ -3,9 +3,13 @@ import re
 
 term = Terminal()
 
+def format_bold(text: str):
+    return re.sub(r'\*\*(.*?)\*\*', r'{term.black_on_snow3}\1{term.normal}', text)
+
+
 def clean_brackets(text: str):
     # Handle triple brackets for SCP links - convert to color format
-    text = re.sub(r'\[\[\[(.*?)\]\]\]', r'{term.firebrick}\1{term.normal}', text)
+    text = re.sub(r'\[\[\[(.*?)\]\]\]', r'{term.black_on_firebrick}\1{term.normal}', text)
 
     # remove regular double brackets and their content
     text = re.sub(r'\[\[(.*?)\]\]', '', text)
@@ -17,7 +21,22 @@ def clean_brackets(text: str):
     return text
 
 
-def formatted_print(metadata, scp: str):
+def format_separator_line(line):
+    # Check if line consists only of 2 or more hyphens and nothing else
+    if line.strip() and all(char == '-' for char in line.strip()):
+        if len(line.strip()) >= 2:
+            # Return a line of '=' that spans the full terminal width
+            return f"\n\n{term.grey_reverse}" + '=' * term.width + f"{term.normal}\n\n"
+    return line
+
+
+def clean_arrow(text: str):
+    if text.strip() == ">":
+        return "\n"
+    return text
+
+
+def formatted_print(metadata: str, scp: str):
     lines = scp.split("\n")
     lines = [x for x in lines if x.strip()]
 
@@ -29,7 +48,13 @@ def formatted_print(metadata, scp: str):
         # Remove all meta-elements of the page
         line = clean_brackets(line)
 
-        # Bold any double-astrixed text
-        line = re.sub(r'\*\*(.*?)\*\*', r'{term.black_on_firebrick}\1{term.normal}', line)
+        # Bold any double-asterisked text
+        line = format_bold(line)
+        
+        # Format any section break
+        line = format_separator_line(line)
+        
+        # Clean all logs with empty lines
+        line = clean_arrow(line)
 
-        print(line.format(term=term) + "\n")
+        print(line.format(term=term) + "\n",)
